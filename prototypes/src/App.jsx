67f26f078ec7c079ab29f9fa1aee9de11d761bad
Ali Rehan <rehanshahii109@gmail.com>
2025-07-29 13:27:06 +0500

    about page

--- prototypes/src/App.jsx
+++ prototypes/src/App.jsx
@@ -3,6 +3,7 @@
 import './App.css'
 import Navber from './components/Navber';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 
 function App() {
@@ -12,8 +13,10 @@ return (
   <>
    <Navber title="TextUtils"/>
    <div className="container my-3">
-   <TextForm heading="Enter the text to analyze" />
-   </div>
+    <TextForm heading="Enter the text to analyze" /> 
+    </div>
+     <About></About>
+  
  </>
 );
 } 
